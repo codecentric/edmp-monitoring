57adcea1a3e3303d7d2dc5d996d6fc32ccd48efd
tobiasflohre <tobias.flohre@codecentric.de>
2016-02-11 23:03:23 +0100

    Initial

---
+++ src/main/java/de/codecentric/EdmpMonitoringApplication.java
@@ -0,0 +1,12 @@
+package de.codecentric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdmpMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdmpMonitoringApplication.class, args);
+	}
+}
