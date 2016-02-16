3ef69f144f5b5b76f632be4627b2a5c5e79c182b
tobiasflohre <tobias.flohre@codecentric.de>
2016-02-16 09:06:28 +0100

    test

--- src/main/java/de/codecentric/EdmpMonitoringApplication.java
+++ src/main/java/de/codecentric/EdmpMonitoringApplication.java
@@ -3,6 +3,9 @@ package de.codecentric;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@EnableAdminServer
 @SpringBootApplication
 public class EdmpMonitoringApplication {
 
